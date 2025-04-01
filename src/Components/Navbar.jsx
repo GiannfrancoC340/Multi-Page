ed4e2fbd2d09a16b10599b6a3bc6d67d414f8f82
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-03-31 21:30:22 -0400

    Added more code to implement functionality

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "../App.css"; // Import styles
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
